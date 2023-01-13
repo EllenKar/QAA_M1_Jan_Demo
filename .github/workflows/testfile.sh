dfffb3ccc55023bf4d8e31784c537ccd64919a4f
EllenKar <122531753+EllenKar@users.noreply.github.com>
2023-01-13 14:24:22 +0000

    Update testfile.sh

--- .github/workflows/testfile.sh
+++ .github/workflows/testfile.sh
@@ -1,10 +1,22 @@
 # the below is necessary to run a linux script (.sh)
 #! /bin/bash
-
+  jobs:
+    action_1
   # clones the repo
-  run: actions/checkout@v1
-  run: touch newfile.txt
-  run: rm newfile.txt
-  run: mkdir new_file
-  run: chmod +x buildScript.sh
-  run: ./buildScript.sh 
+      run: actions/checkout@v1
+      run: touch newfile.txt
+      run: rm newfile.txt
+      run: mkdir new_file
+  
+      run: chmod +x buildScript.sh
+      run: ./buildScript.sh 
+      
+    action_2
+      run: actions/checkout@v1
+      # Below prints out current location
+      run: pwd
+      name: print content
+      run: echo "hello world" >> hello.txt
+
+      run: chmod +x buildScript.sh
+      run: ./buildScript.sh 
